43e0ec0dc870d0852956bbc08e8bba949aafff4c
k23109 <k23109kk@aitech.ac.jp>
2024-12-18 13:42:33 +0900

    weaponCount
    weaponの種類別にカウントしhtmlにそれぞれ送られるようにしました。

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from models import initialize_database, Character, Weapon, Quest
 from routes import blueprints
 from peewee import fn, JOIN
+from models import Weapon
 
 app = Flask(__name__)
 
@@ -22,7 +23,22 @@ def index():
         .group_by(Character)
         .order_by(fn.COUNT(Quest.id).desc())
     )
-    return render_template('index.html', ranking=ranking)#test中
+    weapon_attribute_query = (
+        Weapon
+        .select(Weapon.attribute, fn.COUNT(Weapon.id).alias('attribute_count'))
+        .group_by(Weapon.attribute)
+    )
+
+    weapon_counts = {
+    result.attribute: result.attribute_count
+    for result in weapon_attribute_query
+    }
+
+    sword_count = weapon_counts.get("剣", 0)
+    tue_count = weapon_counts.get("杖", 0)
+
+
+    return render_template('index.html', ranking=ranking,weapon_counts=weapon_counts,sword_count=sword_count,tue_count=tue_count)#test中
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
