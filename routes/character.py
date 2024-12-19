cd4ebaeca03cc8d8661fcdbfff6b6c32e8afd51f
K2277 <k22077kk@aitech.ac.jp>
2024-12-19 11:02:45 +0900

    character.pyの修正
    おそらくテスト用に使われていたものが直っていなかったので修正しました。

--- routes/character.py
+++ routes/character.py
@@ -6,21 +6,11 @@ character_bp = Blueprint('character', __name__, url_prefix='/characters')
 
 @character_bp.route('/')
 def list():
+    
     # データ取得
     characters = Character.select()
 
-    # 性別ごとの人数をカウント
-    male_count = characters.where(Character.gender == '男子').count()
-    female_count = characters.where(Character.gender == '女子').count()
-    other_count = characters.where(Character.gender != '男子', Character.gender != '女子').count()
-
-    # 総数を計算
-    total_count = male_count + female_count + other_count
-
-    return render_template('index.html', title='キャラクター一覧', items=characters,
-                            male_count=male_count, female_count=female_count, other_count=other_count,
-                            total_count=total_count)
-
+    return render_template('character_list.html', title='キャラクター一覧', items=characters)
 
 
 @character_bp.route('/add', methods=['GET', 'POST'])
